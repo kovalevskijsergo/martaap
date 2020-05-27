e7191eca3a3e18a43c2d05689ac33ce101feea55
kovalevskijsergo <36783141+kovalevskijsergo@users.noreply.github.com>
2020-05-27 21:55:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(function(){
         vertical:true,
         dots: true,
         dotsClass:'header-dots',
-        autoplay: 2000,
-        
+        autoplay: 2000     
     });
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    })
 });
